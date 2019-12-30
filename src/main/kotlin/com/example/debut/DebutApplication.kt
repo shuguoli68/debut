b0126793d0b86e8749b80549e34bea7f112232a1
shu <shuguoli68@163.com>
2019-12-30 21:50:53 +0800

    修改打包war

--- src/main/kotlin/com/example/debut/DebutApplication.kt
+++ src/main/kotlin/com/example/debut/DebutApplication.kt
@@ -1,18 +1,14 @@
 package com.example.debut
 
-import com.example.debut.controller.DiaryController
-import com.example.debut.controller.UserController
 import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @MapperScan("com.example.debut.mapper")
 @SpringBootApplication
-class DebutApplication
+class DebutApplication : SpringBootServletInitializer()
 
 fun main(args: Array<String>) {
     runApplication<DebutApplication>(*args)
-    //开启各个Controller
-//    UserController()
-//    DiaryController()
 }
