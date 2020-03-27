2470b2ec9c15e41f22c4f9a10d5e66914ccc589e
shu <shuguoli68@163.com>
2020-03-27 16:51:57 +0800

    完成phone的操作

--- src/main/kotlin/com/example/debut/controller/PhoneController.kt
+++ src/main/kotlin/com/example/debut/controller/PhoneController.kt
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
-@Api(value = "Phone", tags = arrayOf("操作Phone"))
+@Api(value = "Phone", tags = arrayOf("操作Phone，设备信息"))
 @RestController
 class PhoneController {
 
