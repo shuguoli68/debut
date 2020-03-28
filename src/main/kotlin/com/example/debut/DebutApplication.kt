5054b80adfcd8cedb2a6da3efe974c7a4d2a3307
shu <shuguoli68@163.com>
2020-03-28 13:02:35 +0800

    修改打包

--- src/main/kotlin/com/example/debut/DebutApplication.kt
+++ src/main/kotlin/com/example/debut/DebutApplication.kt
@@ -3,6 +3,7 @@ package com.example.debut
 import com.example.debut.util.updown.FileStorageProperties
 import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
@@ -16,4 +17,9 @@ class DebutApplication : SpringBootServletInitializer()
 
 fun main(args: Array<String>) {
     runApplication<DebutApplication>(*args)
+}
+
+@Override
+fun configure(builder:SpringApplicationBuilder) : SpringApplicationBuilder {
+    return builder.sources(DebutApplication::class.java)
 }
